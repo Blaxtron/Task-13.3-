53d34a17183677cc6bb7edef93a55e5c6851b80e
Blaxtron <abdellaahmed15@yahoo.com>
2024-09-17 01:51:17 +0300

    Renamed file and modified and added some comments.

--- Task 13.3/Codes and sources/NN Trainer.py
+++ Task 13.3/Codes and sources/NN Trainer.py
@@ -4,13 +4,13 @@ import network
 # Load MNIST data
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
-# Initialize the network
+# initialize the network by making a network object
 net = network.Network([784, 30, 10])
 
-# Train the network
+# train the network using the SGD method from network.py
 net.SGD(training_data, epochs=30, mini_batch_size=10, eta=3.0, test_data=test_data)
 
-# Evaluate the network
+# store the accuracy of the NN
 accuracy = net.evaluate(test_data)
 
 # Print the accuracy to the console
